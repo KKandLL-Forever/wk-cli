5e1f57f23e22635cfd8e0edbff6bee83b507629f
KKandLL-Forever <clay1wang@gmail.com>
2023-02-04 01:39:37 +0800

    feat: add template

--- src/assets/repository.ts
+++ src/assets/repository.ts
@@ -2,5 +2,7 @@ export default {
   'react@17':
     'direct:https://github.com/KKandLL-Forever/react-template/archive/refs/heads/master.zip',
   'vue3+pinia+router':
+    'direct:https://github.com/KKandLL-Forever/react-template/archive/refs/heads/master.zip',
+  'react17+vite+unocss+ts':
     'direct:https://github.com/KKandLL-Forever/react-template/archive/refs/heads/master.zip'
 };
