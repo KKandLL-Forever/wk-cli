a2729203b0ed27005db08033728da5defe4add68
wangkai <clay1wang@gmail.com>
2023-02-26 22:39:32 +0800

    feat: add next13 template

---
+++ src/template/next13-unocss-ts/unocss.config.ts
@@ -0,0 +1,9 @@
+// unocss.config.ts
+import { defineConfig, presetAttributify, presetIcons, presetUno, presetWebFonts } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    // ...
+  ],
+})
